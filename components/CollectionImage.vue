401977bf2ac738a82c6713b8d86bbdfe71331832
Zainab Khayat <zainab224498@gmail.com>
2025-02-05 01:22:43 +0300

    Update CollectionImage.vue

--- components/CollectionImage.vue
+++ components/CollectionImage.vue
@@ -1,7 +1,8 @@
 <template>
     <section class="my-5">
         <div>
-            <img src="../public/images/Untitled.jpg" class="w-100" />
+            <!--<img src="/images/Untitled.jpg" class="w-100" /> -->
+            <h1>hello</h1>
         </div>
     </section>
 </template>
